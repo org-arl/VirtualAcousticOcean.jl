fd4cb0d82d681aeb1fb220cb80192983e06eb89d
Mandar Chitre <mandar@nus.edu.sg>
2025-01-20 01:52:19 +0800

    feat: implement UASP2

--- examples/2-node-network.jl
+++ examples/2-node-network.jl
@@ -4,8 +4,8 @@ using UnderwaterAcoustics
 env = UnderwaterEnvironment()
 pm = PekerisRayModel(env, 7)
 sim = Simulation(pm, 24000.0)
-addnode!(sim, (0.0, 0.0, -10.0), UASP, 9809)
-addnode!(sim, (1000.0, 0.0, -10.0), UASP, 9819)
+addnode!(sim, (0.0, 0.0, -10.0), UASP2, 9809)
+addnode!(sim, (1000.0, 0.0, -10.0), UASP2, 9819)
 run(sim)
 
 close(sim)
