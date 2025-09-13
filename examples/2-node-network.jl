cbe3e5166e1059f10d44691b8837378389ea0773
Mandar Chitre <mandar@nus.edu.sg>
2025-09-13 17:15:42 +0800

    perf: add multithreading and caching

--- examples/2-node-network.jl
+++ examples/2-node-network.jl
@@ -1,15 +1,15 @@
 using VirtualAcousticOcean
 using UnderwaterAcoustics
+using Sockets
 
-env = UnderwaterEnvironment(seabed=SandyClay, bathymetry=40.0)
+env = UnderwaterEnvironment(
+  bathymetry = 40.0,
+  seabed = SandyClay
+)
 pm = PekerisRayTracer(env)
 sim = Simulation(pm, 24000.0)
 addnode!(sim, (0.0, 0.0, -10.0), UASP2, 9809)
 addnode!(sim, (1000.0, 0.0, -5.0), UASP2, 9819)
 run(sim)
 
-while true
-  sleep(10)
-end
-
-#close(sim)
+wait()
